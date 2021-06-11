fb14ef52575ce984d37d97b4627c21cd5a92e29e
CrystallizedSyrup <jovanko5828@gmail.com>
2021-06-11 07:20:20 +0700

    finalfinal

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,7 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $rooms = Room::with(['hotel'])->orderBy('room_ordered', 'desc')->take(2)->get();
+        $rooms = Room::with(['hotel'])->orderBy('room_ordered', 'desc')->get();
         return view("home", [
             'rooms' => $rooms,
         ]);
