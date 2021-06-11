161759b33963c737e6c65751cd4fdf1c8796df52
CrystallizedSyrup <jovanko5828@gmail.com>
2021-06-11 07:35:22 +0700

    finalfinal

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,9 +9,11 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $rooms = Room::with(['hotel'])->take(2)->get();
+        $rooms = Room::with(['hotel'])->orderBy('room_ordered', 'desc')->limit(1)->paginate(1);
+        $rooms2 = Room::with(['hotel'])->orderBy('room_ordered', 'desc')->offset(1)->limit(1)->paginate(1);
         return view("home", [
             'rooms' => $rooms,
+            'rooms2' => $rooms2
         ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,55 +9,62 @@
 
     </div>
     <section data-aos="fade-left" data-aos-offset="200" data-aos-delay="50" data-aos-duration="1000" class="bg-white">
+        @foreach ($rooms2 as $room)
+
         <div class="max-w-5xl px-6 py-16 mx-auto">
             <div class="items-center md:flex md:space-x-6">
                 <div class="md:w-1/2">
-                    <h3 class="text-2xl font-semibold text-gray-800">{{ $rooms[0]->hotel->name }} </h3><br>
-                    <h4 class="font-light">{{ $rooms[0]->name }}</h4>
+                    <h3 class="text-2xl font-semibold text-gray-800">{{ $room->hotel->name }} </h3><br>
+                    <h4 class="font-light">{{ $room->name }}</h4>
 
-                    <p class="max-w-md mt-4 text-gray-600">Facility Included are: <br>{{ $rooms[0]->facility }}</p>
+                    <p class="max-w-md mt-4 text-gray-600">Facility Included are: <br>{{ $room->facility }}</p>
                 </div>
 
                 <div class="mt-8 md:mt-0 md:w-1/2">
                     <div class="flex items-center justify-center">
                         <div class="max-w-md">
                             <img class="object-cover object-center w-full rounded-md shadow" style="height: 500px;"
-                                src="{{Storage::disk('s3')->url($rooms[0]->room_image) }}">
+                                src="{{Storage::disk('s3')->url($room->room_image) }}">
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        @endforeach
     </section>
 
     <section data-aos="fade-right" data-aos-offset="200" data-aos-delay="50" data-aos-duration="1000" class="bg-white">
+        @foreach ($rooms2 as $room)
+            
         <div class="max-w-5xl px-6 py-16 mx-auto">
             <div class="items-center md:flex md:space-x-6">
                 <div class="hidden md:block md:w-1/2">
                     <div class="flex items-center justify-center">
                         <div class="max-w-md">
                             <img class="object-cover object-center w-full rounded-md shadow" style="height: 500px;"
-                                src="{{ Storage::disk('s3')->url($rooms[1]->room_image) }}">
+                                src="{{ Storage::disk('s3')->url($room->room_image) }}">
                         </div>
                     </div>
                 </div>
 
                 <div class="mt-8 md:mt-0 md:w-1/2">
-                    <h3 class="text-2xl font-semibold text-gray-800">{{ $rooms[1]->hotel->name }} </h3><br>
-                    <h4 class="font-light">{{ $rooms[1]->name }}</h4>
+                    <h3 class="text-2xl font-semibold text-gray-800">{{ $room->hotel->name }} </h3><br>
+                    <h4 class="font-light">{{ $room->name }}</h4>
                     </h3>
-                    <p class="max-w-md mt-4 text-gray-600">Facility Included are:<br>{{ $rooms[1]->facility }}</p>
+                    <p class="max-w-md mt-4 text-gray-600">Facility Included are:<br>{{ $room->facility }}</p>
                 </div>
                 <div class="block md:hidden md:w-1/2">
                     <div class="flex items-center justify-center">
                         <div class="max-w-md">
                             <img class="object-cover object-center w-full rounded-md shadow" style="height: 500px;"
-                                src="{{ Storage::disk('s3')->url($rooms[1]->room_image) }}">
+                                src="{{ Storage::disk('s3')->url($room->room_image) }}">
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        @endforeach
+
     </section>
 
 
